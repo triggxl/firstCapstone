3964052bfeacd3b8c1aa1ecb9e7620a21a2111d8
triggxl <triggxl@hotmail.com>
2020-08-01 19:07:04 -0400

    updated mobile first responsiveness, more semantics to text box, added error handling for input

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ const fetchIex = (individualStockName) => {
  
 //Grab data from promise + DOM manipulation
 const displayResultsIex = (responseJson) => {
-  console.log(responseJson);
   $('.display-results').removeClass('hidden').empty();
   let stockSymbol = responseJson.quote.symbol;
   let companyName = responseJson.quote.companyName;
   let realTimePrice = responseJson.quote.iexRealtimePrice;
-  // let openingPrice = responseJson.quote.open;
   let closingPrice = responseJson.quote.iexClose;
   let dailyPlusOrMinus = responseJson.quote.change;
   let week52High = responseJson.quote.week52High;
@@ -30,44 +28,25 @@ const displayResultsIex = (responseJson) => {
 
 const buildNewsStory = (newsArticle) => {
   return `<div class="newsArticleResults"><h2>${newsArticle.headline}</h2> <div>${newsArticle.summary}</div>
-  <p class="newsArticleImageContainer"></p><a href="${newsArticle.url}"><img class="newsArticleImage"src="${newsArticle.image}">${newsArticle.url}:</a></div>
+  <p class="newsArticleImageContainer"></p><a href="${newsArticle.url}"><img class="newsArticleImage" alt="This is a news story result based on the Stock symbol the user searched for"src="${newsArticle.image}">${newsArticle.url}:</a></div>
   `
 }
 
 const watchSearchingForIndividualStocks = () => {
   $('.submit-form').on('submit', (e) => {
-    console.log('Submit working');
     e.preventDefault();
     let individualStockName = $('#company').val();
+    if (!individualStockName) {
+      alert("Please Enter a Stock Symbol ie: AMZN, IBM, AAPL");
+      return false;
+    }
     //calling fetch passing value to turn into responseJson and display in DOM
     fetchIex(individualStockName).then(responseJson => displayResultsIex(responseJson));
-    // fetchIex(result);
   })
 }
 
 $(function() {
-  console.log('App loaded! Waiting for submit!');
   watchSearchingForIndividualStocks();
 });
-
-
-
-
-/* 
-event handler/setInterval
-fetch
-DOM manipulation
-//methods for DOM manipulation
-*/
-
-//is there a tool that can do what I need, find that tool, use it
-//DOM manipulation, fetches, iteration techniques
-//array iteration methods
-//filter, reduce, forEach, find
-//objects and arrays
-//[{a:1},{a:2}].map(x=>x.a)
-
-
-
 
 
